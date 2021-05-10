6e39ab97300b2c52d10f26330f1fb10617c3c048
Nishad Saraf <nishadsaraf@gmail.com>
2021-05-09 22:56:38 -0700

    gatsby-config: Update website name and description
    Update website name and description.
    
    Signed-off-by: Nishad Saraf <nishadsaraf@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Nishad Saraf',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Nishad Saraf is an Embedded Software Engineer who specializes in building Linux drivers.',
+    siteUrl: 'https://nishadsaraf.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@nishad_saraf',
   },
   plugins: [
     {
@@ -28,8 +28,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'NishadSaraf',
+        short_name: 'NishadSaraf',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
