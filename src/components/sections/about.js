b5612f4fac90edbb462663d04656fa6408ae35a1
Nishad Saraf <nishadsaraf@gmail.com>
2021-05-14 20:10:01 -0700

    src: components: Add about me
    Add about me.
    
    Signed-off-by: Nishad Saraf <nishadsaraf@gmail.com>

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,14 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = [
+    'C',
+    'C++',
+    'Driver Development',
+    'Linux Kernel Hacking',
+    'Operating System',
+    'HW/SW Debug Tools',
+  ];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,33 +142,23 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              I am an open-source enthusiast with the passion and endurance to deliver highly
+              versatile low-level libraries and drivers. I have a wide array of experience, ranging
+              from printing custom circuit boards leading to creating Android applications that
+              interact with IoT devices. I take great care in system programming and user experience
+              of the things I work on.
             </p>
-
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
-            </p>
-
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
+              I have completed my master’s in Electrical and Computer Engineering with a
+              specialization in Embedded Systems from{' '}
+              <a href="https://www.pdx.edu/">Portland State University, Oregon</a>. Currently, at
+              Xilinx, I am working on building libraries and drivers for the industry’s first{' '}
+              <a href="https://www.xilinx.com/products/silicon-devices/acap/versal.html">
+                ACAP (Adaptive Compute Acceleration Platform).
               </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              Previously, at Tesla, I have developed validation frameworks for automotive use-case.
             </p>
-
-            <p>Here are a few technologies I’ve been working with recently:</p>
+            <p>Here are a few technologies I've been working with recently:</p>
           </div>
 
           <ul className="skills-list">
