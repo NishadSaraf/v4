1ad6bfad74c99bf175cdb150fbc601f878b934bf
Nishad Saraf <nishadsaraf@gmail.com>
2021-07-31 02:44:16 -0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-7a70d00abd95fb27e3f5.js"
+    "url": "webpack-runtime-f5cf75e4c163b6d64645.js"
   },
   {
     "url": "framework-e132ed928f348cd72eb4.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "921867d9a4c2408dfe64c35e005062a2"
+    "revision": "e17adfcee0531b6c0d136fd7c6a990d8"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b2207d8704b643f0fd7e.js"
